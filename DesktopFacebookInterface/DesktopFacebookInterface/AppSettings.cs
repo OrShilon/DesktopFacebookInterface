01fce5bac419e266f5c747008804e8964fc3a32b
Yoav Barnea <Yoav.Barnea@post.idc.ac.il>
2020-11-22 14:17:41 +0200

    AppSettings

---
+++ DesktopFacebookInterface/DesktopFacebookInterface/AppSettings.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace DesktopFacebookInterface
+{
+    public class AppSettings
+    {
+        public Point WindowLocation { get; set; }
+        public Size WindowSize { get; set; }
+        public bool RememberUser { get; set; }
+        public string UserAccessToken { get; set; }
+
+        readonly static string defaultFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        private AppSettings()
+        {
+            WindowLocation = new Point(900, 500);
+            WindowSize = new Size(700, 400);
+            RememberUser = false;
+            UserAccessToken = null;
+        }
+
+        public void SaveFile()
+        {
+            string path = String.Format(@"{0}\appSettings.xml", defaultFolder);
+            FileMode fileModeToUse = File.Exists(path) ? FileMode.Truncate : FileMode.Create;
+
+            using (Stream stream = new FileStream(path, fileModeToUse))
+            {
+                XmlSerializer serializer = new XmlSerializer(this.GetType());
+                serializer.Serialize(stream, this);
+            }
+        }
+
+        public static AppSettings LoadFile()
+        {
+            AppSettings appSettings;
+            string path = String.Format(@"{0}\\appSettings.xml", defaultFolder);
+            if (File.Exists(path))
+            {
+                using (Stream stream = new FileStream(path, FileMode.Open))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(AppSettings));
+                    appSettings = serializer.Deserialize(stream) as AppSettings;
+                }
+            }
+            else
+            {
+                appSettings = new AppSettings();
+            }
+
+            return appSettings;
+        }
+    }
+}
--- DesktopFacebookInterface/DesktopFacebookInterface/HomeScreen.cs
+++ DesktopFacebookInterface/DesktopFacebookInterface/HomeScreen.cs
@@ -14,13 +14,15 @@ namespace DesktopFacebookInterface
     public partial class HomeScreen : Form
     {
         LoginResult m_LoginResult;
+        AppSettings m_AppSettings;
         User m_LoginUser;
         UserInformationWrapper user;
 
-        public HomeScreen(LoginResult i_LoginResult, User i_LoginUser)
+        public HomeScreen(LoginResult i_LoginResult, User i_LoginUser, AppSettings i_AppSettings)
         {
             m_LoginResult = i_LoginResult;
             m_LoginUser = i_LoginUser;
+            m_AppSettings = i_AppSettings;
             user = new UserInformationWrapper(m_LoginUser);
             //Console.WriteLine(m_LoginUser.Name);
             InitializeComponent();
@@ -28,6 +30,15 @@ namespace DesktopFacebookInterface
             PictureBoxCoverPhoto.LoadAsync(user.CoverImage);
             this.ShowDialog();
             //PictureBoxCoverPhoto.Load(m_LoginUser.Cover.SourceURL);
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.Manual;
+            this.Size = m_AppSettings.WindowSize;
+            this.Location = m_AppSettings.WindowLocation;
+
+            base.OnShown(e);
         }
 
         private void HomeScreen_Load(object sender, EventArgs e)
@@ -44,6 +55,30 @@ namespace DesktopFacebookInterface
         {
             m_LoginUser = null;
             MessageBox.Show("You are now logged out!!");
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            m_AppSettings.WindowLocation = this.Location;
+            m_AppSettings.WindowSize = this.Size;
+
+            if (m_AppSettings.RememberUser)
+            {
+                m_AppSettings.UserAccessToken = m_LoginResult.AccessToken;
+            }
+            else
+            {
+                m_AppSettings.UserAccessToken = null;
+            }
+
+            m_AppSettings.SaveFile();
+
+            base.OnFormClosing(e);
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            base.OnFormClosed(e);
         }
     }
 }
